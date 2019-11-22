f77df41c684b384c3d0497e92003ac6d71cd0990
Louise Linné <louise.linne@afconsult.com>
2019-11-22 13:57:41 +0100

    added web interface with login and register forms and other pages. Register is functional, Login is not

--- server.js
+++ server.js
@@ -1,12 +1,77 @@
+// Login and registration server
 const express = require('express');
 const app = express(); // rest requests
 const bcrypt= require('bcrypt');
 
 app.use(express.json()) // allows application to accept json
+app.use(express.urlencoded({extended: false})) // so we can access form inputs specified ejs file as req.body.password for example (based on name field of <input>)
+
 const users = []; // replace with DB
 
-app.get('/users', (req, res) => {
-    res.json(users)
+// questions and thoughts
+// what is res and why does it have functions
+// read about express and ejs
+// chose database type
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: 'Louise'}); // assing along
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+    const user = users.find(user => req.body.email == user.email);
+
+    if (user) {
+        console.log("user exists");
+        try {
+            if (true) {//await bcrypt.compare(req.body.password, user.password)) {
+                res.send('Success');
+            } else {
+                res.send('Invalid password');
+            }
+        } catch {
+            res.status(500).send();
+        }
+    } else {
+        console.log("user not exists")
+        res.send('Invalid user');
+    }
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        console.log('hashedPassword: ', hashedPassword);
+
+        const user = {
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        };
+        users.push(user); // add to DB
+
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register');
+    }
+    console.log("users: ", users)
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.ejs');
+})
+
+app.get('/users', (req, res) => { // will display all users on localhost:3000/users in json
+    res.json(users);
 });
 
 // request and respons
@@ -16,7 +81,7 @@ app.post('/users', async (req, res) => { // bcrypt is an asynchronous library
         // console.log('salt: ', salt)
         // const hashedPassword = await bcrypt.hash(req.body.password, salt); // await async
         const hashedPassword = await bcrypt.hash(req.body.password, 10); // equivalent to doing salt seperately
-        console.log('hashedPassword: ', hashedPassword)
+        console.log('hashedPassword: ', hashedPassword);
         // hashed password will contain the salt and the hash
 
         const user = { 
@@ -34,7 +99,7 @@ app.post('/users', async (req, res) => { // bcrypt is an asynchronous library
 app.post('/users/login', async (req, res) => {
     const givenName = req.body.name;
     const givenPassword = req.body.password;
-    const user = users.find(user => user.name == givenName)
+    const user = users.find(user => user.name == givenName);
 
     console.log('USER: ', user)
     if (!user) {
@@ -42,13 +107,13 @@ app.post('/users/login', async (req, res) => {
     }
     try {
         if (await bcrypt.compare(givenPassword, user.password)) { // prevents timing attacks
-            console.log('-------')
+            console.log('-------');
             res.send('Success'); // default status 200
         } else {
             res.send('Incorrect user or password');
         }
     } catch {
-        res.status(500).send()
+        res.status(500).send();
     }
 })
 
